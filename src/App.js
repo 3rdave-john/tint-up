6baa20f3da415a7a6b1425df1385739c08ebe37c
John Yu <john@3rdave.com>
2018-03-07 10:46:06 -0800

    added standard css too index.css, added samsung avatar, created a dummy app layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import avatar from './assets/images/samsung.gif';
+
+// avatar is width 354px, height 354px
+// web image width 305px, height 305px
 
 class App extends Component {
   render() {
@@ -7,7 +11,8 @@ class App extends Component {
       <div className="App">
         <div className="albums">
           <div class="album">
-            <img class="album__artwork" src="https://source.unsplash.com/random/300x300?v=1"/>
+            {/* <img class="album__artwork" src="https://source.unsplash.com/random/300x300?v=1"/> */}
+            <img class="album__artwork" alt="" src={avatar}/>
             <div class="album__details">
               <h2>Album Title</h2>
               <p class="album__artist">Artist Name</p>
