e2a74aa2a3944800a716e689d184aae53a9a5075
John Yu <john@3rdave.com>
2018-03-08 13:55:03 -0800

    added new eslint rule, created grid layout for avatar, added new GIF, changed css in Avatar.css

--- src/Avatar.jsx
+++ src/Avatar.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import avatar from './assets/images/samsung.gif';
-
+import avatar from './assets/images/samsung1.gif';
+import avatar1 from './assets/images/samsung.gif';
+import './Avatar.css';
 // avatar is width 354px, height 354px
 // web image width 305px, height 305px
 
 const Avatar = () => (
   <div className="albums">
     <div className="album">
-      {/* <img class="album__artwork" src="https://source.unsplash.com/random/300x300?v=1"/> */}
       <img className="album__artwork" alt="" src={avatar} />
-      <div className="album__details">
-        <h2>Album Title</h2>
-        <p className="album__artist">Artist Name</p>
-        <p className="album__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sed sint doloremque repellat, iste debitis.</p>
-        <p className="album__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, excepturi!</p>
-      </div>
+      <img className="album__artwork" alt="" src={avatar1} />
+      <img className="album__artwork" alt="" src={avatar} />
+      <img className="album__artwork" alt="" src={avatar1} />
+      <img className="album__artwork" alt="" src={avatar} />
+      <img className="album__artwork" alt="" src={avatar1} />
     </div>
   </div>
 );
